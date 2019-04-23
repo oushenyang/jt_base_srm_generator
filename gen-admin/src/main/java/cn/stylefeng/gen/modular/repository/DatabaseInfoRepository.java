3d25c35207a419dae6eb2295d6054c76db8b0331
fengshuonan <sn93@qq.com>
2019-04-23 14:44:17 +0800

    代码生成器

---
+++ gen-admin/src/main/java/cn/stylefeng/gen/modular/repository/DatabaseInfoRepository.java
@@ -0,0 +1,10 @@
+package cn.stylefeng.gen.modular.repository;
+
+import cn.stylefeng.gen.modular.model.DatabaseInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by stephan on 20.03.16.
+ */
+public interface DatabaseInfoRepository extends JpaRepository<DatabaseInfo, Long> {
+}
